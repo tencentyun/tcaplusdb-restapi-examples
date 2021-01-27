2fabd862c2db1e6127de6e93f4eb18567e803e15
ballenwen <ballenwen@tencent.com>
2021-01-27 21:03:18 +0800

    update traverse

--- traverse.sh
+++ traverse.sh
@@ -7,27 +7,60 @@ source ./global.sh
 function build_traverse_data(){
     cat <<EOF
 {
-    "Limit": 3,
-    "TraveseSession":"0.0",
-    "ReturnValues": "user buffers, to build a bridge between request and response, use case: global object that do not want to save in memory"
+    "Limit": 100,
+    "TraveseSession": "0.0",
+    "ReturnValues": "user buffers"
+}
+EOF
+}
+function build_traverse_loop_data(){
+    cat <<EOF
+{
+    "Limit": 1,
+    "TraveseSession": "$1",
+    "ReturnValues": "user buffers"
 }
 EOF
 }
 
 #traverse all records of table, specify `Limit` attr to limit numbers of record of response
-#if table has massive data, each response size has a limit level (10M-level), if the size of records is over this limit
-# user should check the return flag `complete` to check whether the records have been traversed
-# if `complete` flag is false, use for-loop to traverse the remaining records
-# best practice: combine `Limit` attr and `complete` flag to control number of records of each response and to check number of remaining records
 function traverse() {
   target="Tcaplus.Traverse"
   build_headers "$target"
   curl -i -XPOST "${headers[@]}" "$rest_url" -d "$(build_traverse_data)"
   echo -e "\n"
 }
+# traverse all records of table, use loop to traverse
+# loop check condition: TraveseSession and Complete
+# TraveseSession: Traverse session of each traverse, and use to locate the offset position of next traverse
+# Complete: Check whether the loop is terminated or not, False means continue, True means end
+function traverse_loop() {
+  target="Tcaplus.Traverse"
+  build_headers "$target"
+  complete="False"
+  limit=1
+  traverse_session="0.0"
+  total_num=0
+  while [ "$complete" == "False" ];do 
+      ret=$(curl -i -XPOST "${headers[@]}" "$rest_url" -d "$(build_traverse_loop_data $traverse_session)" | grep -v HTTP | grep -v "json-1.0" | grep -v "content-length" | grep -v "^$" )
+      echo "$ret"
+      
+      complete=$( echo "$ret" | python3 -c "import sys, json; print(json.load(sys.stdin)['Complete'])" )
+      traverse_session=$( echo "$ret" | python3 -c "import sys, json; print(json.load(sys.stdin)['TraveseSession'])" )
+
+      if [ "$complete" == "False" ];then
+
+          total_num=$(($total_num+1))
+      fi
+      
+  done
+  echo "TotalRecords:$total_num"
+}
 
 #declare array variable
 declare -a headers
+#build request headers
+build_headers
 
-
-traverse
+#traverse
+traverse_loop
